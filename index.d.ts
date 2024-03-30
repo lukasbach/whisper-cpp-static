e42cb168bbe0fd8f18052affe46b669c981a7830
Lukas Bach <contact@lukasbach.com>
2024-03-30 17:42:01 +0100

    typings

---
+++ index.d.ts
@@ -0,0 +1,21 @@
+type Exports = {
+    jar: string,
+    dll: {
+        x64: string,
+        x86: string
+    },
+    bin: {
+        win: {
+            x64: string,
+            x86: string
+        }
+    },
+    blas: {
+        win: {
+            x64: string,
+            x86: string
+        }
+    }
+};
+
+export default Exports;
